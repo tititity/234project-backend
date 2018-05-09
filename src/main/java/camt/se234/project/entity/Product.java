63757d0dccd5765b48f6ea111fb02a5963017d1f
Unknown <spsr5032@gmail.com>
2018-05-10 00:43:31 +0700

    testGetAverageSaleOrderPrice
    add uni test to getAverageSaleOrder method

--- src/main/java/camt/se234/project/entity/Product.java
+++ src/main/java/camt/se234/project/entity/Product.java
@@ -25,4 +25,40 @@ public class Product {
     String imageLocation;
     double price;
 
+    public Product(Long id, String productId, String name, String description, String imageLocation, double price){
+        this.id = id;
+        this.productId= productId;
+        this.name = name;
+        this.description = description;
+        this.imageLocation = imageLocation;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageLocation() {
+        return imageLocation;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
