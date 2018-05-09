8b96ccf27daf1368e24279a8d99cfde4921efb96
Unknown <game.080.pf@gmail.com>
2018-05-09 23:11:55 +0700

    test method getAllProduct passed

--- src/main/java/camt/se234/project/dao/ProductDaoImpl.java
+++ src/main/java/camt/se234/project/dao/ProductDaoImpl.java
@@ -11,11 +11,24 @@ import java.util.stream.StreamSupport;
 
 @Repository
 public class ProductDaoImpl implements ProductDao{
+    List<Product> products;
+    public ProductDaoImpl(){
+        products.add(new Product(201L,"23","game","description","image",40.56));
+        products.add(new Product(202L,"24","titi","description","image",45.56));
+        products.add(new Product(203L,"26","pen","description","image",34.75));
+        products.add(new Product(204L,"27","fan","description","image",85.89));
+        products.add(new Product(205L,"28","bag","description","image",55.60));
+    }
     @Autowired
     ProductRepository productRepository;
     @Override
     public List<Product> getProducts() {
         return StreamSupport.stream(productRepository.findAll().spliterator(),false)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return null;
     }
 }
---
+++ src/test/java/camt/se234/project/service/ProductServiceImplTests.java
@@ -0,0 +1,39 @@
+package camt.se234.project.service;
+
+import camt.se234.project.dao.ProductDao;
+import camt.se234.project.entity.Product;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ProductServiceImplTests  {
+    ProductDao productDao;
+    ProductServiceImpl productService;
+
+    @Before
+    public void setup(){
+        productDao = mock(ProductDao.class);
+        productService = new ProductServiceImpl();
+        productService.setProductDao(productDao);
+    }
+
+    @Test
+    public void testGetAllProducts(){
+        ProductServiceImpl productService = new ProductServiceImpl();
+        productService.setProductDao(productDao);
+        List<Product> mockProducts = new ArrayList<>();
+        mockProducts.add(new Product(201L,"23","game","description","image",40.56));
+        mockProducts.add(new Product(202L,"24","titi","description","image",45.56));
+        mockProducts.add(new Product(203L,"26","pen","description","image",34.75));
+        mockProducts.add(new Product(204L,"27","fan","description","image",85.89));
+        mockProducts.add(new Product(205L,"28","bag","description","image",55.60));
+        when(productDao.findAll()).thenReturn(mockProducts);
+        assertThat(productService.getAllProducts());
+    }
+}
