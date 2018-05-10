b557c0eae228e66b2d16a6e394ce115594b31815
Unknown <game.080.pf@gmail.com>
2018-05-10 22:04:08 +0700

    delete method test in test order

--- src/test/java/camt/se234/project/service/SaleOrderServiceImplTests.java
+++ src/test/java/camt/se234/project/service/SaleOrderServiceImplTests.java
@@ -18,19 +18,6 @@ import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 
 public class SaleOrderServiceImplTests {
-    @Test
-    public void testGetSaleOders(){
-        OrderDao orderDao = mock(OrderDao.class);
-        List<SaleOrder> mockSaleOrder = new ArrayList<>();
-        SaleOrderServiceImpl saleOrderService = new SaleOrderServiceImpl();
-        saleOrderService.setOrderDao(orderDao);
-
-        mockSaleOrder.add(new SaleOrder(001L,"o001"));
-        mockSaleOrder.add(new SaleOrder(002L, "o002"));
-        mockSaleOrder.add(new SaleOrder(003L, "o003"));
-        when(orderDao.getOrders()).thenReturn(mockSaleOrder);
-
-        assertThat(saleOrderService.getSaleOrders(),hasItems(new SaleOrder(001L,"o001"),
-                new SaleOrder(002L,"o002"), new SaleOrder(003L,"o003")));
-    }
+    
 }
+
