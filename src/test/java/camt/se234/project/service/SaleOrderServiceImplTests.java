2471fc48324323ff9c001014c5d44f424695eef6
Unknown <game.080.pf@gmail.com>
2018-05-10 20:03:15 +0700

    Add import project

--- src/test/java/camt/se234/project/service/SaleOrderServiceImplTests.java
+++ src/test/java/camt/se234/project/service/SaleOrderServiceImplTests.java
@@ -1,43 +1,36 @@
 package camt.se234.project.service;
 
 import camt.se234.project.dao.OrderDao;
-import camt.se234.project.entity.Product;
 import camt.se234.project.entity.SaleOrder;
-import camt.se234.project.entity.SaleTransaction;
-import org.hamcrest.Matcher;
-import org.junit.Before;
+import camt.se234.project.service.SaleOrderServiceImpl;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+
 public class SaleOrderServiceImplTests {
-    OrderDao orderDao;
-    SaleOrderServiceImpl saleOrderService;
-
-    @Before
-    public void setup(){
-        orderDao = mock(OrderDao.class);
-        saleOrderService = new SaleOrderServiceImpl();
-        saleOrderService.setOrderDao(orderDao);
-    }
-
     @Test
-    public void testGetSaleOrder(){
-        List<SaleTransaction> transactions = new ArrayList<>();
-        transactions.add(new SaleTransaction("A",new SaleOrder("5555",transactions),
-                new Product(203L,"26","pen","description","image",34.75)));
-        List<SaleOrder> saleOrders = new ArrayList<>();
-        saleOrders.add(new SaleOrder("5555",transactions));
-        when(orderDao.getOrders()).thenReturn(saleOrders);
-        assertThat(saleOrderService.getSaleOrders(),hasItem(new SaleOrder("5555",transactions)));
-    }
+    public void testGetSaleOders(){
+        OrderDao orderDao = mock(OrderDao.class);
+        List<SaleOrder> mockSaleOrder = new ArrayList<>();
+        SaleOrderServiceImpl saleOrderService = new SaleOrderServiceImpl();
+        saleOrderService.setOrderDao(orderDao);
 
-    private void assertThat(List<SaleOrder> saleOrders, Matcher<Iterable<? super SaleOrder>> iterableMatcher) {
+        mockSaleOrder.add(new SaleOrder(001L,"o001"));
+        mockSaleOrder.add(new SaleOrder(002L, "o002"));
+        mockSaleOrder.add(new SaleOrder(003L, "o003"));
+        when(orderDao.getOrders()).thenReturn(mockSaleOrder);
+
+        assertThat(saleOrderService.getSaleOrders(),hasItems(new SaleOrder(001L,"o001"),
+                new SaleOrder(002L,"o002"), new SaleOrder(003L,"o003")));
     }
 }
